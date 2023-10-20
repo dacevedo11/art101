05aa34048d8955cb697dd0d88e6158e4048c277e
Daniel Acevedo <69832074+dacevedo11@users.noreply.github.com>
2023-10-20 13:53:24 -0700

    completed most of lab 4:)

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 4 - Pseudocoding and Problem-solving
+// Author: Daniel Acevedo <daceved4@ucsc.edu>
+// Date: October 20, 2023
 
-// Constants
+// BLACKJACK
 
-// Functions
+// Dealer shuffles cards
+// Each player gets 2 cards
+// PLAYER'S TURN
+  // Add sum of cards
+  // If sums of cards is less than 21, continue
+    // Player can hit or stay
+    // If player stays, go to dealer's turn
+  // If sum is greater than or equal to game is over
+  // If sum is 21 or less, continue to dealers turn
+// DEALERS TURN
+  // Add sum of cards
+  // Check if 17 or greater
+    // If yes, dealer must stay
+    // If no, dealer must hit
+  // If no one busts, whoever has higher total wins 
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
