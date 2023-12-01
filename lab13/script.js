8a538f2341cf2b506054171c4d5398a209df30a6
Daniel Acevedo <dacevedo23@gmail.com>
2023-11-30 17:02:03 -0800

    finished lab 14

--- lab13/script.js
+++ lab13/script.js
@@ -1,33 +1,29 @@
-// Lab 13 Loops
-// Daniel Acevedo 
-// Nov 27 2023
-
-
 function fizzBuzz() {
-    var result = "";
+    var longString = "";
     for (var i = 1; i <= 200; i++) {
-        if (i % 3 == 0) {
-            result += "Fizz";
-        }
-        if (i % 5 == 0) {
-            result += "Buzz";
-        }
-        if (i % 7 == 0) {
-            result += "Boom";
-        }
-        if (!(i % 3 == 0 || i % 5 == 0 || i % 7 == 0)) {
-            result += i + " ";
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+            longString += i + ".) FizzBuzzBoom!<br>";
+        } else if (i % 3 == 0 && i % 5 == 0) {
+            longString += i + ".) FizzBuzz!<br>";
+        } else if (i % 3 == 0 && i % 7 == 0) {
+            longString += i + ".) FizzBoom!<br>";
+        } else if (i % 5 == 0 && i % 7 == 0) {
+            longString += i + ".) BuzzBoom!<br>";
+        } else if (i % 3 == 0) {
+            longString += i + ".) Fizz!<br>";
+        } else if (i % 5 == 0) {
+            longString += i + ".) Buzz!<br>";
+        } else if (i % 7 == 0) {
+            longString += i + ".) Boom!<br>";
+        } else {
+            longString += i + ".) ----- <br>";
         }
     }
-    return result;
+    return longString;
 }
 
+
 $("#button").click(function () {
-    $("#output").html('<div class="text"><p>' + fizzBuzz() + '</p></div>');
+    $("#output").html(fizzBuzz());
+    $("#output-box").toggleClass("hidden");
 });
-
-
-
-
-
-
