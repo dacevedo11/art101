46cd1e2b2ec067fd237de67a4e0b5a58c08d7f1e
Daniel Acevedo <dacevedo23@gmail.com>
2023-12-03 14:23:41 -0800

    added links to lab 15

--- lab15/script.js
+++ lab15/script.js
@@ -2,19 +2,6 @@
 // Name: Daniel Acevedo <daceved4@ucsc.edu>
 // Date: December 3, 2023
 
-// Function to generate a random number within a specified range
-function getRandomBerryId(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  // Define the range of valid berry IDs
-  const minBerryId = 1; // Assuming 1 is the minimum valid ID
-  const maxBerryId = 50; // Adjust this based on the actual maximum valid ID
-  
-  // Generate a random berry ID
-  const randomBerryId = getRandomBerryId(minBerryId, maxBerryId);
-
-
 $("#activate").click(function () {
     $.ajax(
         {
